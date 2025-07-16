13481f36594cd3fb16ef1d773156459b7c0cb1d7
vicky-voin <alteneria127@gmail.com>
2025-07-16 11:24:54 -0400

    Made htmls into ejs views; added a footer

--- index.js
+++ index.js
@@ -3,42 +3,19 @@ const path = require("node:path");
 
 const app = express();
 
-const fileOptions = {
-    root: __dirname
-};
+const assetPath = path.join(__dirname, "public");
+
+app.use(express.static(assetPath));
 
 app.set('views', path.join(__dirname, "views"));
 app.set('view engine', "ejs");
 
-app.get("/", (req, res) => serveMain(res));
-app.get("/about", (req, res) => serveAbout(res));
-app.get("/contact-me", (req, res) => serveContact(res));
+app.get("/", (req, res) => res.render("index"));
+app.get("/about", (req, res) => res.render("about", {title:"About"}));
+app.get("/contact-me", (req, res) => res.render("contact-me"));
 
-app.use((req, res, next) => serve404(res));
+app.use((req, res, next) => res.render("404"));
 
 const PORT = 8080;
 
 app.listen(PORT);
-
-function serveMain(result){
-    returnPage('index.html', result);
-}
-
-function serveAbout(result){
-    result.render("about", {title:"About"});
-}
-
-function serveContact(result){
-    returnPage('contact-me.html', result);
-}
-
-function serve404(result)
-{
-    result.status(404);
-    returnPage('404.html', result);
-}
-
-function returnPage(pageName, result)
-{
-    result.sendFile(pageName, fileOptions);
-}
